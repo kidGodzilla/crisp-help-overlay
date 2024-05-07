d7d97c6094674f6973c825f7c8be680664477409
senhajichihab <chihabsenhaji@gmail.com>
2024-05-07 03:12:06 +0100

    updates

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import simpleCommandComponentWithShortcut from "@/components/simpleCommandCompon
 import testCommand from "@/components/testCommand.vue"
 </script>
 <template>
-    <simpleCommandComponent />
-    <simpleCommandComponentWithShortcut />
-    <testCommand />
+    <simpleCommandComponent /> <!--Simple Command Component With No Shortcuts-->
+    <simpleCommandComponentWithShortcut />  <!--Same as above but with Shortcuts for open/close dialog-->
+    <testCommand /> <!--Edited so I can display the crisp data-->
 </template>
